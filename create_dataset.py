7511582ce9b76fc43697d4f3c5ff5ebc591a0af4
Tyler Parker <btylerparker@gmail.com>
2019-01-12 16:53:10 -0500

    Proper split, hdf5

--- create_dataset.py
+++ create_dataset.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import h5py
 import cv2
+from PIL import Image
+from sklearn.utils import shuffle
 
 DETECTION_CLASSES = ["fire"]
 TRAIN_CLASSES = ["background"] + DETECTION_CLASSES
@@ -11,16 +13,46 @@ TRAIN_CLASSES = ["background"] + DETECTION_CLASSES
 IMG_DIMENSIONS = (738, 960)
 IMG_SHAPE = (*IMG_DIMENSIONS, 3)
 
+PADDING_FILL_COLOR=(127, 127, 127)
+
 def train_validate_test_split(df, train_percent, validate_percent, seed=42):
-    np.random.seed(seed)
-    perm = np.random.permutation(df.index)
-    m = len(df.index)
-    train_end = int(train_percent * m)
-    validate_end = int(validate_percent * m) + train_end
-    train = df.ix[perm[:train_end]]
-    validate = df.ix[perm[train_end:validate_end]]
-    test = df.ix[perm[validate_end:]]
+    df = shuffle(df, random_state=seed)
+    train_end = int(train_percent * len(df))
+    validate_end = train_end + int(validate_percent * len(df))
+    train = df[:train_end]
+    validate = df[train_end:validate_end]
+    test = df[validate_end:]
     return train, validate, test
+
+def process_img(img):
+
+    # Convert to PIL format
+    img = resize_contain_img(
+        img=img,
+        desired_dimensions=IMG_DIMENSIONS,
+    )
+    # cv2.imshow("image", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return img
+
+def resize_contain_img(img, desired_dimensions):
+    orig_h, orig_w, _ = img.shape
+    des_h, des_w = desired_dimensions
+
+    ratio = max(des_h, des_w)/ float(max(orig_h, orig_w))
+
+    new_size = tuple([int(x*ratio) for x in [orig_h, orig_w]])
+    new_h, new_w = new_size
+
+    img = cv2.resize(img, (new_w, new_h))
+
+    delta_w = des_w - new_w
+    delta_h = des_h - new_h
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    new_img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=PADDING_FILL_COLOR)
+    return new_img
 
 class CreateDataset(cli.Application):
     DESCRIPTION = "Create a dataset"
@@ -28,23 +60,67 @@ class CreateDataset(cli.Application):
     train_percent = cli.SwitchAttr(
         ['--train'],
         argtype=float,
-        help='Path to desired input moshcap ledgers',
+        help='Dataset parition percentage for training',
         default=0.6,
     )
     
     validate_percent = cli.SwitchAttr(
         ['--validate'],
         argtype=float,
-        help='Path to desired input moshcap ledgers',
+        help='Dataset parition percentage for validation',
         default=0.2,
     )
 
     output = cli.SwitchAttr(
         ['--output'],
         argtype=str,
-        help='Path to desired dataset output folder',
+        help='Output directory',
         mandatory=True,
     )
+
+    def create_split_dataset(self, split_type, df, hdf5_file):
+        pkl_filepath = os.path.join(self.output, "{}.pkl".format(split_type))
+        df.to_pickle(pkl_filepath)
+        print("- {}, {} items, {}".format(
+            split_type,
+            len(df),
+            pkl_filepath,
+        ))
+        shape = (len(df), *IMG_SHAPE)
+
+        labels = df['label_index'].values
+        image_paths = df['image'].values
+
+        hdf5_file.create_dataset("{}_img".format(split_type), shape, np.int8)
+
+        if split_type == 'train':
+            hdf5_file.create_dataset("train_mean", IMG_SHAPE, np.float32)
+
+        hdf5_file.create_dataset("{}_labels".format(split_type), (len(df),), np.int8)
+        hdf5_file["{}_labels".format(split_type)][...] = labels
+
+        if split_type == 'train':
+            # a numpy array to save the mean of the images
+            mean = np.zeros(IMG_SHAPE, np.float32)
+
+        for i, image_path in enumerate(image_paths):
+            # print how many images are saved every 1000 images
+            if i % 100 == 0 and i > 1:
+                print('{} data: {}/{}'.format(split_type, i, len(image_paths)))
+            
+            img = cv2.imread(image_path)
+            img = process_img(img=img)
+
+            # cv2 load images as BGR, convert it to RGB
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # add any image pre-processing here
+            # save the image and calculate the mean so far
+            hdf5_file["{}_img".format(split_type)][i, ...] = img[None]
+            if split_type == 'train':
+                mean += img / float(len(labels))
+        if split_type == 'train':
+            hdf5_file["train_mean"][...] = mean
 
     def main(self, *ledgers):
         if not os.path.exists(self.output):
@@ -55,111 +131,18 @@ class CreateDataset(cli.Application):
             dataframes.append(pd.read_pickle(ledger))
 
         df = pd.concat(dataframes)
-
+        print("Creating dataset '{}', total {} items:".format(self.output, len(df)))
         train_df, validate_df, test_df = train_validate_test_split(
             df=df,
             train_percent=self.train_percent,
             validate_percent=self.validate_percent,
         )
 
-        train_pkl_filepath = os.path.join(self.output, "train.pkl")
-        validate_pkl_filepath = os.path.join(self.output, "validate.pkl")
-        test_pkl_filepath = os.path.join(self.output, "test.pkl")
-
-        train_df.to_pickle(train_pkl_filepath)
-        validate_df.to_pickle(validate_pkl_filepath)
-        test_df.to_pickle(test_pkl_filepath)
-
-        print("Creating dataset '{}', total {} items:".format(self.output, len(df)))
-        print("- Train ({}%) {} items, {}".format(
-            round(self.train_percent * 100, 2),
-            len(train_df),
-            train_pkl_filepath,
-        ))
-        print("- Validate ({}%) {} items, {}".format(
-            round(self.validate_percent * 100, 2),
-            len(validate_df),
-            validate_pkl_filepath,
-        ))
-        print("- Test ({}%) {} items, {}".format(
-            round((1.0 - self.train_percent - self.validate_percent) * 100, 2),
-            len(test_df),
-            test_pkl_filepath,
-        ))
-
-        train_shape = (len(train_df), *IMG_SHAPE)
-        validate_shape = (len(validate_df), *IMG_SHAPE)
-        test_shape = (len(test_df), *IMG_SHAPE)
-
         hdf5_path = os.path.join(self.output, "dataset.hdf5")
         hdf5_file = h5py.File(hdf5_path, mode='w')
-
-        train_labels = train_df['label_index'].values
-        validate_labels = validate_df['label_index'].values
-        test_labels = test_df['label_index'].values
-
-        train_image_paths = train_df['image'].values
-        validate_image_paths = validate_df['image'].values
-        test_image_paths = test_df['image'].values
-
-        hdf5_file.create_dataset("train_img", train_shape, np.int8)
-        hdf5_file.create_dataset("validate_img", validate_shape, np.int8)
-        hdf5_file.create_dataset("test_img", test_shape, np.int8)
-        hdf5_file.create_dataset("train_mean", IMG_SHAPE, np.float32)
-        hdf5_file.create_dataset("train_labels", (len(train_df),), np.int8)
-        hdf5_file["train_labels"][...] = train_labels
-        hdf5_file.create_dataset("validate_labels", (len(validate_df),), np.int8)
-        hdf5_file["validate_labels"][...] = validate_labels
-        hdf5_file.create_dataset("test_labels", (len(test_df),), np.int8)
-        hdf5_file["test_labels"][...] = test_labels
-
-        # a numpy array to save the mean of the images
-        mean = np.zeros(IMG_SHAPE, np.float32)
-        # loop over train addresses
-        for i in range(len(train_image_paths)):
-            # print how many images are saved every 1000 images
-            if i % 100 == 0 and i > 1:
-                print('Train data: {}/{}'.format(i, len(train_image_paths)))
-            # read an image and resize
-            # cv2 load images as BGR, convert it to RGB
-            addr = train_image_paths[i]
-            img = cv2.imread(addr)
-            img = cv2.resize(img, IMG_DIMENSIONS[::-1], interpolation=cv2.INTER_CUBIC)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            # add any image pre-processing here
-            # save the image and calculate the mean so far
-            hdf5_file["train_img"][i, ...] = img[None]
-            mean += img / float(len(train_labels))
-        # loop over validation addresses
-        for i in range(len(validate_image_paths)):
-            # print how many images are saved every 1000 images
-            if i % 1000 == 0 and i > 1:
-                print('Validation data: {}/{}'.format(i, len(validate_image_paths)))
-            # read an image and resize
-            # cv2 load images as BGR, convert it to RGB
-            addr = validate_image_paths[i]
-            img = cv2.imread(addr)
-            img = cv2.resize(img, IMG_DIMENSIONS[::-1], interpolation=cv2.INTER_CUBIC)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            # add any image pre-processing here
-            # save the image
-            hdf5_file["validate_img"][i, ...] = img[None]
-        # loop over test addresses
-        for i in range(len(test_image_paths)):
-            # print how many images are saved every 1000 images
-            if i % 1000 == 0 and i > 1:
-                print('Test data: {}/{}'.format(i, len(test_image_paths)))
-            # read an image and resize
-            # cv2 load images as BGR, convert it to RGB
-            addr = test_image_paths[i]
-            img = cv2.imread(addr)
-            img = cv2.resize(img, IMG_DIMENSIONS[::-1], interpolation=cv2.INTER_CUBIC)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            # add any image pre-processing here
-            # save the image
-            hdf5_file["test_img"][i, ...] = img[None]
-        # save the mean and close the hdf5 file
-        hdf5_file["train_mean"][...] = mean
+        self.create_split_dataset(split_type='train', df=train_df, hdf5_file=hdf5_file)
+        self.create_split_dataset(split_type='validate', df=validate_df, hdf5_file=hdf5_file)
+        self.create_split_dataset(split_type='test', df=test_df, hdf5_file=hdf5_file)
         hdf5_file.close()
         print("...Dataset saved to '{}'".format(hdf5_path))
 
